058957a4e00defdb7f30e5f3fd0ce5df82d51281
BEN ARIEL FRANÇA MARTINS <fadomarshemello@gmail.com>
2024-12-06 10:14:22 -0300

    adição do rencohecimento de caracteres especiais e correção de palavras reservadas

--- LEX2.py
+++ LEX2.py
@@ -1,36 +1,35 @@
 import ply.lex as lex
 from ply.lex import TOKEN
-import re
-# List of token names.   This is always required
+
 tokens = (
-   'NUMBER',
-   'PLUS',
-   'MINUS',
-   'TIMES',
-   'DIVIDE',
-   'LPAREN',
-   'RPAREN',
-   'NOT',
-   'AND',
-   'OR',
-   'SOME',
-   'ALL',
-   'VALUE',
-   'MIN',
-   'MAX',
-   'EXACTLY',
-   'THAT',
-   'EquivalenteTo',
-   'Individuals',
-   'SubClassOf',
-   'DisjointClasses',
-   'IndividualNames',
+    'NUMBER',
+    'PLUS',
+    'MINUS',
+    'TIMES',
+    'DIVIDE',
+    'LPAREN',
+    'RPAREN',
+    'NOT',
+    'AND',
+    'OR',
+    'SOME',
+    'ALL',
+    'VALUE',
+    'MIN',
+    'MAX',
+    'EXACTLY',
+    'THAT',
+    'EquivalentTo',
+    'Individuals',
+    'SubClassOf',
+    'DisjointClasses',
+    'IndividualNames',
+    'SpecialCharacters',
 )
 
 reserved = {
-    'not': 'NOT', 
+    'not': 'NOT',
     'and': 'AND',
-    'NOT' : 'NOT',
     'or': 'OR',
     'some': 'SOME',
     'all': 'ALL',
@@ -39,84 +38,90 @@ reserved = {
     'max': 'MAX',
     'exactly': 'EXACTLY',
     'that': 'THAT',
-    'class': 'Class',
-    'equivalenteto' : 'EquivalenteTo',
-    'individuals' : 'Individuals',
-    'subclassof' : 'SubClassOf',
-    'disjointclasses': 'DisjointClasses', 
+    'equivalentto:': 'EquivalentTo',
+    'individuals:': 'Individuals',
+    'subclassof:': 'SubClassOf',
+    'disjointclasses:': 'DisjointClasses',
 }
 
 
 RESERVED_GERAL = r'\b([Nn][Oo][Tt]|[Aa][Nn][Dd]|[Oo][Rr]|[Ss][Oo][Mm][Ee]|[Aa][Ll][Ll]|[Vv][Aa][Ll][Uu][Ee]|[Mm][Ii][Nn]|[Mm][Aa][Xx]|[Ee][Xx][Aa][Cc][Tt][Ll][Yy]|[Tt][Hh][Aa][Tt])\b'
-RESERVED_OTHERS = r'\b(EquivalenteTo|Individuals|SubClassOf|DisjointClasses)\b'
-INDIVIDUALS_NAMES = r'\b[A-Z][a-z].*\d\b'
+RESERVED_OTHERS = r'(EquivalentTo:|Individuals:|SubClassOf:|DisjointClasses:)'
+INDIVIDUALS_NAMES = r'\b[A-Z][a-zA-Z]*\d+\b'
+SPECIAL_CARACTERES = r'[\(\)\[\]\{\}\,\<\>\"]'
 
-t_PLUS    = r'\+'
-t_MINUS   = r'-'
-t_TIMES   = r'\*'
-t_DIVIDE  = r'/'
-t_LPAREN  = r'\('
-t_RPAREN  = r'\)'
+t_PLUS = r'\+'
+t_MINUS = r'-'
+t_TIMES = r'\*'
+t_DIVIDE = r'/'
+def t_NUMBER(t):
+    r'\b(\d+)\b'
+    t.value = int(t.value)
+    t.lexer.num_numbers+=1
+    return t
 
-
-#FUNÇÕES DAS PALAVRAS RESERVADAS
 @TOKEN(RESERVED_GERAL)
 def t_RESERVED_GERAL(t):
-    t.type = reserved[t.value.lower()] 
+    t.type = reserved[t.value.lower()]
     t.lexer.num_reserved += 1
     return t
 
 @TOKEN(RESERVED_OTHERS)
 def t_RESERVED_OTHERS(t):
-    t.type = reserved[t.value.lower()] 
+    t.type = reserved[t.value.lower()]
     t.lexer.num_reserved += 1
     return t
+
 
 @TOKEN(INDIVIDUALS_NAMES)
 def t_INDIVIDUAL_NAMES(t):
     t.lexer.num_individual_names += 1
-    t.type="IndividualNames"
+    t.type = "IndividualNames"
     return t
 
-
-
+@TOKEN(SPECIAL_CARACTERES)
+def t_SPECIAL_CHARACTERS(t):
+    t.type = "SpecialCharacters"
+    t.lexer.num_special_characters += 1
+    return t
 
 def t_newline(t):
     r'\n+'
     t.lexer.lineno += len(t.value)
 
-# A string containing ignored characters (spaces and tabs)
-t_ignore  = ' \t'
-
+t_ignore = ' \t'
 
 def t_COMMENT(t):
     r'\#.*'
     pass
 
 def t_error(t):
-    print("Illegal character '%s'" % t.value[0])
+    print(f"Illegal character '{t.value[0]}'")
     t.lexer.skip(1)
 
 # Build the lexer
 lexer = lex.lex(debug=True)
 lexer.num_reserved = 0
-lexer.num_individual_names=0
-
+lexer.num_individual_names = 0
+lexer.num_special_characters = 0
+lexer.num_numbers=0
 
 data = '''
-AAAAAAAAAAAriel12FDFDFDFD     12ARIEL
-BenAriel12
+{AAAAAAAAAAAriel12FDFDFDFD    Ariel12 12ARIEL
+<BenAriel}12>  ["5"] aaaaaEquivalentTo:aaa Individuals: SubClassOf: DisjointClasses:
 '''
 
-# Give the lexer some input
+
 lexer.input(data)
 
 # Tokenize
 while True:
     tok = lexer.token()
     if not tok:
-        break      # No more input
+        break
     print(tok)
 
-print(lexer.num_reserved)
-print(lexer.num_individual_names)
+print("Number of Reserved Words:", lexer.num_reserved)
+print("Number of Individual Names:", lexer.num_individual_names)
+print("Number of Special Characters:", lexer.num_special_characters)
+print("Number of Numbers:", lexer.num_numbers)
