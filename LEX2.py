cc261366bc4380f0b94cbb58a38457d0546a2bf3
BEN ARIEL FRANÇA MARTINS <fadomarshemello@gmail.com>
2024-12-06 11:02:16 -0300

    leitura de arquivo texto e exibição do bloco deo erro inteiro

--- LEX2.py
+++ LEX2.py
@@ -44,7 +44,6 @@ reserved = {
     'disjointclasses:': 'DisjointClasses',
 }
 
-
 RESERVED_GERAL = r'\b([Nn][Oo][Tt]|[Aa][Nn][Dd]|[Oo][Rr]|[Ss][Oo][Mm][Ee]|[Aa][Ll][Ll]|[Vv][Aa][Ll][Uu][Ee]|[Mm][Ii][Nn]|[Mm][Aa][Xx]|[Ee][Xx][Aa][Cc][Tt][Ll][Yy]|[Tt][Hh][Aa][Tt])\b'
 RESERVED_OTHERS = r'(EquivalentTo:|Individuals:|SubClassOf:|DisjointClasses:)'
 INDIVIDUALS_NAMES = r'\b[A-Z][a-zA-Z]*\d+\b'
@@ -57,7 +56,7 @@ t_DIVIDE = r'/'
 def t_NUMBER(t):
     r'\b(\d+)\b'
     t.value = int(t.value)
-    t.lexer.num_numbers+=1
+    t.lexer.num_numbers += 1
     return t
 
 @TOKEN(RESERVED_GERAL)
@@ -71,7 +70,6 @@ def t_RESERVED_OTHERS(t):
     t.type = reserved[t.value.lower()]
     t.lexer.num_reserved += 1
     return t
-
 
 @TOKEN(INDIVIDUALS_NAMES)
 def t_INDIVIDUAL_NAMES(t):
@@ -96,21 +94,33 @@ def t_COMMENT(t):
     pass
 
 def t_error(t):
-    print(f"Illegal character '{t.value[0]}'")
-    t.lexer.skip(1)
+    # Captura o início da sequência ilegal
+    illegal_sequence = t.value[0]
+    i = 1
+    
+    # Continua capturando enquanto os caracteres não forem reconhecidos e não forem espaços
+    while i < len(t.value) and not t.value[i].isspace() and not any(t.value[i:].startswith(p) for p in ['+', '-', '*', '/', '(', ')']):
+        illegal_sequence += t.value[i]
+        i += 1
+    
+    # Reporta a sequência inteira como um erro
+    print(f"Illegal sequence: '{illegal_sequence}' at line {t.lineno}")
+    
+    # Avança o lexer pelo tamanho da sequência ilegal
+    t.lexer.skip(len(illegal_sequence))
+
 
 # Build the lexer
 lexer = lex.lex(debug=True)
 lexer.num_reserved = 0
 lexer.num_individual_names = 0
 lexer.num_special_characters = 0
-lexer.num_numbers=0
+lexer.num_numbers = 0
 
-data = '''
-{AAAAAAAAAAAriel12FDFDFDFD    Ariel12 12ARIEL
-<BenAriel}12>  ["5"] aaaaaEquivalentTo:aaa Individuals: SubClassOf: DisjointClasses:
-'''
-
+# Reading data from a file
+file_path = 'input.txt'
+with open(file_path, 'r') as file:
+    data = file.read()
 
 lexer.input(data)
 
