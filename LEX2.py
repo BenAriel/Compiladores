0ccf3dc17251174c56d043e7a1b1c501147a69d6
BEN ARIEL FRANÇA MARTINS <fadomarshemello@gmail.com>
2024-12-09 08:02:08 -0300

    correção dos datatypes

--- LEX2.py
+++ LEX2.py
@@ -19,6 +19,7 @@ tokens = (
     'MAX',
     'EXACTLY',
     'THAT',
+    'string',
     'EquivalentTo',
     'Individuals',
     'SubClassOf',
@@ -75,6 +76,8 @@ reserved = {
     'individuals:': 'Individuals',
     'subclassof:': 'SubClassOf',
     'disjointclasses:': 'DisjointClasses',
+}
+namespacesAndTypes= {
     'owl:' : 'owl',
     'rdf:' : 'rdf',
     'rdfs:' : 'rdfs',
@@ -97,6 +100,7 @@ reserved = {
     'hexBinary' : 'hexBinary',
     'int' : 'int',
     'integer' : 'integer',
+    'string' : 'string',
     'languague' : 'languague',
     'long' : 'long',
     'Name' : 'Name',
@@ -109,10 +113,10 @@ reserved = {
 RESERVED_GERAL = r'\b([Nn][Oo][Tt]|[Aa][Nn][Dd]|[Oo][Rr]|[Ss][Oo][Mm][Ee]|[Aa][Ll][Ll]|[Vv][Aa][Ll][Uu][Ee]|[Mm][Ii][Nn]|[Mm][Aa][Xx]|[Ee][Xx][Aa][Cc][Tt][Ll][Yy]|[Tt][Hh][Aa][Tt])\b'
 RESERVED_OTHERS = r'(EquivalentTo:|Individuals:|SubClassOf:|DisjointClasses:)'
 INDIVIDUALS_NAMES = r'\b[A-Z][a-zA-Z]*\d+\b'
-SPECIAL_CARACTERES = r'[\(\)\[\]\{\}\,\<\>\"]'
+SPECIAL_CARACTERES = r'[\(\)\[\]\{\}\,\<\>\"\=]'
 class_identifier = r'\b[A-Z][a-zA-Z]*(\s[A-Z][a-zA-Z])*\b'
 property_identifier = r'\b([a-z][a-zA-Z]*|is[A-Z][a-zA-Z]*Of|has[A-Z][a-zA-Z])\b'
-data_type = r'\b(owl:|rfg:|rdfs:|xsd:|rational|real|langString|PlainLiteral|XMLLiteral|Literal|anyURI|base64Binary|boolean|byte|dateTime|dateTimeStamp|decimal|double|float|hexBinary|int|integer|language|long|Name|NCName|negativeInteger|NMTOKEN|nonNegativeInteger)\b'
+data_type = r'\b(owl:|rfg:|rdfs:|xsd:|rational|real|langString|PlainLiteral|XMLLiteral|Literal|anyURI|base64Binary|boolean|byte|dateTime|dateTimeStamp|decimal|double|float|hexBinary|int|integer|language|long|Name|NCName|negativeInteger|NMTOKEN|nonNegativeInteger|string)\b'
 
 t_PLUS = r'\+'
 t_MINUS = r'-'
@@ -138,12 +142,13 @@ def t_RESERVED_OTHERS(t):
 
 @TOKEN(data_type)
 def t_DATA_TYPE(t):
-    t.type = reserved[t.value.lower()] 
-    if t.value == 'owl:' or t.value == 'xsd:' or t.value == 'rdfs' or t.value == 'rdf':
+    t.type = namespacesAndTypes[t.value.lower()] 
+    if t.value == 'owl:' or t.value == 'xsd:' or t.value == 'rdfs:' or t.value == 'rdf:':
         t.value = 'NAMESPACEID'
+        t.lexer.num_namespace_ids += 1
     else: 
         t.value = 'DATA_TYPE'
-    t.lexer.num_reserved += 1
+        t.lexer.num_data_types += 1
     return t
 
 @TOKEN(property_identifier)
@@ -154,12 +159,12 @@ def t_PROPERTY_IDENTIFIER(t):
         t.type = "PROPERTY_IDENTIFIER_is_Of"
     else:
         t.type = "PROPERTY_IDENTIFIER"
-    t.lexer.num_reserved += 1
+    t.lexer.num_property_identifiers += 1
     return t
 
 @TOKEN(class_identifier)
 def t_CLASS_IDENTIFIER(t):
-    t.lexer.num_reserved += 1
+    t.lexer.num_class_identifiers += 1
     return t
 
 @TOKEN(INDIVIDUALS_NAMES)
@@ -202,11 +207,15 @@ def t_error(t):
 
 
 # Build the lexer
-lexer = lex.lex(debug=True)
+lexer = lex.lex()
 lexer.num_reserved = 0
 lexer.num_individual_names = 0
 lexer.num_special_characters = 0
 lexer.num_numbers = 0
+lexer.num_namespace_ids = 0
+lexer.num_data_types = 0
+lexer.num_property_identifiers = 0
+lexer.num_class_identifiers = 0
 
 # Reading data from a file
 file_path = 'input.txt'
@@ -226,3 +235,8 @@ print("Number of Reserved Words:", lexer.num_reserved)
 print("Number of Individual Names:", lexer.num_individual_names)
 print("Number of Special Characters:", lexer.num_special_characters)
 print("Number of Numbers:", lexer.num_numbers)
+print("Number of Namespace IDs:", lexer.num_namespace_ids)
+print("Number of Data Types:", lexer.num_data_types)
+print("Number of Property Identifiers:", lexer.num_property_identifiers)
+print("Number of Class Identifiers:", lexer.num_class_identifiers)
+
