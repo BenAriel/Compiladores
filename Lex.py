2cc410704398dd343c4dd7b9575191833eca4144
BEN ARIEL FRANÇA MARTINS <fadomarshemello@gmail.com>
2024-12-10 20:12:48 -0300

    versão final

--- Lex.py
+++ Lex.py
@@ -117,16 +117,22 @@ namespacesAndTypes= {
 }
 # expressçao regular para palavras reservadas que podem ter variações de maiusculas e minusculas,como por exemplo: not, Not, NOT, nOt.
 RESERVED_GERAL = r'\b([Nn][Oo][Tt]|[Aa][Nn][Dd]|[Oo][Rr]|[Ss][Oo][Mm][Ee]|[Aa][Ll][Ll]|[Vv][Aa][Ll][Uu][Ee]|[Mm][Ii][Nn]|[Mm][Aa][Xx]|[Ee][Xx][Aa][Cc][Tt][Ll][Yy]|[Tt][Hh][Aa][Tt])\b'
+
 # expressão regular para palavras reservadas que não tem variacão,tem apenas uma forma de escrever.
 RESERVED_OTHERS = r'(Class:|EquivalentTo:|Individuals:|SubClassOf:|DisjointClasses:)'
+
 # expressão regular para identificar nomes de individuos
 INDIVIDUALS_NAMES = r'\b[A-Z][a-zA-Z]*\d+\b'
+
 # expressão regular para identificar caracteres especiais
 SPECIAL_CARACTERES = r'[\(\)\[\]\{\}\,\<\>\"\=]'
+
 # expressão regular para identificar classes
 CLASS_IDENTIFIER = r'\b[A-Z][a-zA-Z]*(\s[A-Z][a-zA-Z])*\b'
+
 # expressão regular para identificar propriedades
 PROPERTY_IDENTIFIER = r'\b([a-z][a-zA-Z]*|is[A-Z][a-zA-Z]*Of|has[A-Z][a-zA-Z])\b'
+
 # expressão regular para identificar tipos de dados
 DATA_TYPE = r'\b(owl:|rfg:|rdfs:|xsd:|rational|real|langString|PlainLiteral|XMLLiteral|Literal|anyURI|base64Binary|boolean|byte|dateTime|dateTimeStamp|decimal|double|float|hexBinary|int|integer|language|long|Name|NCName|negativeInteger|NMTOKEN|nonNegativeInteger|string)\b'
 
@@ -135,33 +141,29 @@ t_MINUS = r'-'
 t_TIMES = r'\*'
 t_DIVIDE = r'/'
 
-#funções para identificar os tokens. cada função tem uma expressão regular que identifica o token e retorna o token identificado.
+#funções para identificar os tokens. cada função tem uma expressão regular que identifica o token(o que está dentro do parênteses de @token é a expressão regular que identifica aquele token) e retorna o token identificado.
 
-
-
-
+#funçaõ para reconhecer números
 def t_NUMBER(t):
     r'\b(\d+)\b'
     t.value = int(t.value)
     t.lexer.num_numbers += 1
     return t
 
+#função para reconhecer palavras reservadas gerais(que podem ter variações de maiusculas e minusculas)
 @TOKEN(RESERVED_GERAL)
 def t_RESERVED_GERAL(t):
     t.type = reserved[t.value.lower()]
     t.lexer.num_reserved += 1
     return t
-
+#função para reconhecer palavras reservadas que não tem variações de maiusculas e minusculas
 @TOKEN(RESERVED_OTHERS)
 def t_RESERVED_OTHERS(t):
     t.type = reserved[t.value.lower()]
     t.lexer.num_reserved += 1
     return t
 
-# Função que varre o texto lendo e recebendo as letras que formaram o regex, se encontrar retorna true, se não, retornara false
-# Especificamente essa função irá varrer e buscar as palavras do regex de data_type
-# Se encontrar as palavras do regex (owl, xsd, rdfs, rdf) irá editar o tipo para NAMESPACEID
-# Caso seja qualquer outra palavra do regex, irá editar o tipo para DATA_TYPE
+#função para reconhecer tipos de dados.Se encontrar as palavras do regex (owl, xsd, rdfs, rdf) irá editar o tipo para NAMESPACEID. Caso seja qualquer outra palavra do regex, irá editar o tipo para DATA_TYPE
 @TOKEN(DATA_TYPE)
 def t_DATA_TYPE(t):
     t.type = namespacesAndTypes[t.value.lower()]
@@ -173,10 +175,7 @@ def t_DATA_TYPE(t):
         t.lexer.num_data_types += 1
     return t
 
-# Função que varre o texto lendo e recebendo as letras que formaram o regex, se encontrar retorna true, se não, retornara false
-# Especificamente essa função irá varrer e buscar as palavras do regex de property_identifier
-# Se encontrar as palavras do regex (has, is_Of) irá editar o tipo para um property identifier especifico, sendo respectivamente PROPERTY_IDENTIFIER_has ou PROPERTY_IDENTIFIER_is_Of.
-# Caso seja qualquer outra palavra do regex, irá editar o tipo para PROPERTY_IDENTIFIER
+#função para reconhecer nome de propriedades. Se encontrar as palavras do regex começando com "has" ou começando com "is" e terminando com "Of", irá editar o tipo para um property identifier especifico, sendo respectivamente PROPERTY_IDENTIFIER_has ou PROPERTY_IDENTIFIER_is_Of. Caso seja qualquer outra palavra do regex, irá editar o tipo para PROPERTY_IDENTIFIER
 @TOKEN(PROPERTY_IDENTIFIER)
 def t_PROPERTY_IDENTIFIER(t):
     if t.value.startswith("has"):
@@ -188,36 +187,40 @@ def t_PROPERTY_IDENTIFIER(t):
     t.lexer.num_property_identifiers += 1
     return t
 
-# Função que varre o texto lendo e recebendo as letras que formaram o regex, se encontrar retorna true, se não, retornara false
-# Especificamente essa função irá varrer e buscar as palavras do regex de CLASS_IDENTIFIER
+#função para reconhecer identificadores de classes
 @TOKEN(CLASS_IDENTIFIER)
 def t_CLASS_IDENTIFIER(t):
     t.lexer.num_class_identifiers += 1
     return t
 
+#função para reconhecer nomes de individuos
 @TOKEN(INDIVIDUALS_NAMES)
 def t_INDIVIDUAL_NAMES(t):
     t.lexer.num_individual_names += 1
     t.type = "IndividualNames"
     return t
 
+#função para reconhecer caracteres especiais
 @TOKEN(SPECIAL_CARACTERES)
 def t_SPECIAL_CHARACTERS(t):
     t.type = "SpecialCharacters"
     t.lexer.num_special_characters += 1
     return t
 
+# função para reconhecer quebras de linha
 def t_newline(t):
     r'\n+'
     t.lexer.lineno += len(t.value)
 
+# regex para ignorar espaços e tabulações
 t_ignore = ' \t'
 
+# função para ignorar comentários
 def t_COMMENT(t):
     r'\#.*'
     pass
 
-#se o bloco de caracteres não se encaixar em nenhum dos tokens acima, a função de erro é chamada.
+#função de erro. Se um bloco de caracteres não se encaixar em nenhum dos tokens acima, a função de erro abaixo é chamada.
 def t_error(t):
 
     illegal_sequence = t.value[0]
